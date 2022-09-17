3c14e01a05b23704d30b0c74fe7153f2292f9a50
howardreith <howard.reith@gmail.com>
2022-09-17 15:29:15 -0400

    Add updating of status

--- CaseAnalysis/Services/UpdateCaseAnalysis.js
+++ CaseAnalysis/Services/UpdateCaseAnalysis.js
@@ -1,17 +1,19 @@
 import uploadFileToS3 from '../../utils/uploadFileToS3.js';
 import CaseAnalysisModel from '../Models/CaseAnalysisModel.js';
-import { STATUSES } from '../../utils/constants.js';
 
 export default async function updateCaseAnalysis(data, files) {
   const {
-    caseName, notes, id, filesToDelete,
+    caseName, notes, id, filesToDelete, status,
   } = data;
   const caseAnalysisCurrentlyInDb = await CaseAnalysisModel.findById(id);
   let imagesInDb = [...caseAnalysisCurrentlyInDb.images];
   if (filesToDelete) {
     imagesInDb = imagesInDb.filter((image) => !filesToDelete.includes(image.key));
   }
-  const s3Data = await Promise.all((files.map((file) => uploadFileToS3(file))));
+  let s3Data = [];
+  if (files) {
+    s3Data = await Promise.all((files.map((file) => uploadFileToS3(file))));
+  }
   const formattedS3Data = s3Data.map((datum) => ({
     eTag: datum.Etag,
     location: datum.Location,
@@ -19,13 +21,12 @@ export default async function updateCaseAnalysis(data, files) {
   }));
   const finalImagesData = [...imagesInDb, ...formattedS3Data];
   const dataToSave = {
-    caseName,
-    notes,
+    caseName: caseName || caseAnalysisCurrentlyInDb.caseName,
+    notes: notes || caseAnalysisCurrentlyInDb.notes,
+    status: status || caseAnalysisCurrentlyInDb.status,
     images: finalImagesData,
     updatedAt: new Date(),
   };
   await CaseAnalysisModel.findByIdAndUpdate(id, dataToSave);
   return CaseAnalysisModel.findById(id);
-  // const newCaseAnalysis = new CaseAnalysisModel(dataToSave);
-  // return newCaseAnalysis.save();
 }
